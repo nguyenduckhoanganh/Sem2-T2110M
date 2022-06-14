2c324600d4b90980fe51a8f5ae3f21f180367c0c
nguyenduckhoanganh <101682858+nguyenduckhoanganh@users.noreply.github.com>
2022-06-14 19:30:12 +0700

    Add files via upload

---
+++ lap13/LyricWord.java
@@ -0,0 +1,28 @@
+package lap13;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LyricWord {
+    private static final String[] lyric = {"you", "say", "it", "best", "when", "you", "say", "nothing", "at", "all"};
+    public static void main(String[] args) {
+        List words = new ArrayList();
+        for (String w : lyric)
+            words.add(w);
+        for (Object o : words)
+            System.out.print(o + " ");
+        System.out.println("\n------------------");
+
+        System.out.println("Contains [you]?:" + words.contains("you"));
+        System.out.println("Contains [me]?:" + words.contains("me"));
+        System.out.println("Where's [say]?:" + words.indexOf("say"));
+        System.out.println("Where's the last [say]?: " +       words.lastIndexOf("say"));
+        Collections.sort(words);
+        for (Object o : words) {
+            System.out.print(o + " ");
+        }
+    }
+
+
+}
